85568524d5355ff2166bbfb16e1044bc228e7b90
matelig <mateusz1994a@gmail.com>
2017-04-12 22:59:06 +0200

    add assets :D

---
+++ Assets/Scripts/GroundControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundControl : MonoBehaviour {
+    public Renderer rend;
+    // Use this for initialization
+    void Start () {
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+    }
+
+    //Material texture offset rate
+    float speed = .2f;
+
+    //Offset the material texture at a constant rate
+    void Update()
+    {
+        float offset = Time.time * speed;
+        rend.material.mainTextureOffset = new Vector2(0, -offset);
+    }
+}
