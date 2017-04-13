100f9d03e80e5a9cd0904a5f970fd875686a9f29
matelig <mateusz1994a@gmail.com>
2017-04-13 11:01:25 +0200

    first look into spawning objects <not done yet>

---
+++ Assets/Scripts/PowerupScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerupScript : MonoBehaviour
+{
+    public float objectSpeed = -.1f;
+
+    void Update()
+    {
+        transform.Translate(0, 0, objectSpeed);
+    }
+}
