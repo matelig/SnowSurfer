c3e1941cf1f25ffd67b605b93d3451b86a459c9a
alachman <alicja.lachman@euvic.pl>
2017-05-13 14:05:51 +0200

    Added new animation - crash. Fixed jump animation. Reexported model. Added crash anim after colliding with snowman and rocks.

--- Assets/Scripts/ModelCollision.cs
+++ Assets/Scripts/ModelCollision.cs
@@ -3,46 +3,73 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ModelCollision : MonoBehaviour {
+public class ModelCollision : MonoBehaviour
+{
 
     public static int points = 0;
     public Text text;
     CharacterControlScript controlScript;
+    private Animation animator;
     // Use this for initialization
     private void Awake()
     {
-        
+
     }
-    void Start () {
+    void Start()
+    {
         text = GameObject.Find("Coins").GetComponent<Text>();
         GameObject thePlayer = GameObject.Find("model1");
         controlScript = thePlayer.GetComponent<CharacterControlScript>();
+        animator = thePlayer.GetComponent<Animation>();
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     void OnCollisionEnter(UnityEngine.Collision collision)
     {
-        
+
         if ((collision.gameObject.tag == "Coin"))
         {
             points++;
             Destroy(collision.gameObject);
             text.text = points.ToString();
+
         }
-        if ((collision.gameObject.tag=="Snowman")|| (collision.gameObject.tag == "Object"))
+        if ((collision.gameObject.tag == "Snowman") || (collision.gameObject.tag == "Object"))
         {
-            GroundVariables.stop = true;
-            Time.timeScale = 0;
-            controlScript.collided = true;
+            Debug.Log("Kolizja!");
+            StartCoroutine(Collide());
+      
+      
+            //GroundVariables.stop = true;
+            //Time.timeScale = 0;
+            //controlScript.collided = true;
         }
         if ((collision.gameObject.tag == "Wall"))
         {
+            Debug.Log("Kolizja!");
             transform.rotation = Quaternion.Euler(0, 0, 0);
             controlScript.collided = true;
         }
+    }
+
+    IEnumerator Collide()
+    {
+        animator.Play("Armature|crash");
+        yield return new WaitForSeconds(animator["Armature|crash"].length);
+        GroundVariables.stop = true;
+        Time.timeScale = 0;
+        controlScript.collided = true;
+    }
+    private IEnumerator WaitForAnimation(Animation animation)
+    {
+        do
+        {
+            yield return null;
+        } while (animation.isPlaying);
     }
 }
